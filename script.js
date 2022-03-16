2c058f2570641ae7bb152f2b6f2ea5c204f062dc
liameen <liameen2@gmail.comgit config --global init.defaultBranch maingit config --global user.email liameen2@gmail.com>
2022-03-16 18:01:30 +0000

    Fix the pixel generator

--- script.js
+++ script.js
@@ -7,43 +7,23 @@ const container = document.querySelector('.container');
 
 
 
-// // functions to create divs and append them and set id and classes
-// function createDivPixel(){
-//     var div = document.createElement('div');
-//     div.setAttribute('class', `grid-items item${numTag}`);
-//     container.appendChild(div);
-//     numTag++;
-//     return div;
-// }
+// functions to create divs and append them and set id and classes
 
 
+function createDivPixel(cols, rows){
 
-
-
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-      cell.innerText = (c + 1);
-      container.appendChild(cell).className = "grid-item";
-    };
-  };
-  
-  makeRows(12, 12);
-  
+    container.style.setProperty('--grid-rows', rows);
+    
+    for(let i = 0; i < (cols * rows); i++){
+        var div = document.createElement('div');
+    div.setAttribute('class', `grid-item item${numTag}`);
+    container.appendChild(div);
+    numTag++;
+    }
+}
 
-
-
-
-// function pixelCreator(n){
-//     for(let i = 0; i < n; i++){
-//       createDivPixel();
-      
-//     }
-// }
-
-// pixelCreator(256);
+createDivPixel(6, 6);
 
 
 
