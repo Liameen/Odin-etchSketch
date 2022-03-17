2f673b2cab02fb73373dc233720250e1ea852ae3
liameen <liameen2@gmail.comgit config --global init.defaultBranch maingit config --global user.email liameen2@gmail.com>
2022-03-17 14:02:25 +0000

    Fix click eventlistener change div background

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 let numTag = 1;
 
-
 const container = document.querySelector('.container');
 
 
 
 
+
+
+
 // functions to create divs and append them and set id and classes
-
-
 function createDivPixel(cols, rows){
 
     container.style.setProperty('--grid-cols', cols);
@@ -26,5 +26,20 @@ function createDivPixel(cols, rows){
 createDivPixel(6, 6);
 
 
+let pixels = document.querySelectorAll('.grid-item');
+
+console.log(pixels);
+
+
+
+// pixels.addEventListener('click', (e) => {
+//     e.target.style.backgroundColor = '#333';
+// })
+
+ pixels.forEach((pixel) => {
+    pixel.addEventListener('click', (e) => {
+        e.target.style.backgroundColor = '#333';
+    })
+ })
 
 
